4e0db40a1f851d42b4ff99b09ce7a6004980ec3e
noormdafsar <nooruddin.mdafsar.it25@heritageit.edu.in>
2024-10-25 23:33:14 +0530

    DevTinder Frontend

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        
+        <h1>Building Frontend part of Dev Tinder (reference from tinder web app)</h1>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
